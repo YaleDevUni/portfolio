c6b6350d00657214a47f6df6497c48e66b450ae5
YaleDevUni <rhrnak4403@gmail.com>
2023-09-02 20:29:09 +0900

    netflify deployment v1

--- portfolio/src/components/stack_button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function StackButton(props) {
-  return (
-    <div className=" bg-white mx-2  text-black w-fit rounded-md ">
-      <div className=" m-1 font-bold">{props.text}</div>
-    </div>
-  );
-}
-export default StackButton;
